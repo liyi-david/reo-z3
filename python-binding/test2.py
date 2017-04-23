ece30ce567681e3edf4a968a65bf80ac52b4627d
liyi <liyi_math@pku.edu.cn>
2017-04-24 01:40:17 +0800

    :q

---
+++ python-binding/test2.py
@@ -0,0 +1,16 @@
+from reo import *
+
+c1 = Connector()
+c1.connect('Sync', 'A', 'M')
+c1.connect('Fifo1', 'M', 'B')
+c1.connect('Fifo1', 'M', 'C')
+
+
+c2 = Connector()
+c2.connect('Fifo1', 'A', 'N')
+c2.connect('Sync', 'N', 'B')
+c2.connect('Sync', 'N', 'C')
+
+result, counterexample, smt = c1.isRefinementOf(c2, 10)
+
+print result
