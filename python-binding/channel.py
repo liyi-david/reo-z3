18378aebb9103eb10af77ba8a632fb6c5ee6f657
Tune <zhangxiyue@pku.edu.cn>
2017-05-10 14:39:28 +0800

    feat(channel): new merge and lossy channels

--- python-binding/channel.py
+++ python-binding/channel.py
@@ -68,9 +68,43 @@ class Channel:
         return result
     
     @staticmethod
-    def Merge1(nodes, bound):
+    def Lossy1(nodes, idx, num, bound):
+        assert len(nodes) == 2
+        if bound == num:
+            return True
+        constraints_0 = []
+        constraints_1 = []
+        constraints_0 += [ nodes[0]['data'][idx] != nodes[1]['data'][num]]
+        constraints_0 += [ nodes[0]['time'][idx] != nodes[1]['time'][num]]
+        constraints_1 += [ nodes[0]['data'][idx] == nodes[1]['data'][num]]
+        constraints_1 += [ nodes[0]['time'][idx] == nodes[1]['time'][num]]
+        return Or(And(Merge(constraints_0), Lossy1(nodes, idx + 1, num, bound)),
+                  And(Merge(constraints_1), Lossy1(nodes, idx + 1, num + 1, bound)))
+
+    @staticmethod
+    def Merge1(nodes, idx_1, idx_2, bound):
         assert len(nodes) == 3
+        if bound == idx_1 + idx_2:
+            return True
+        constraints_1 = []
+        constraints_2 = []
+        constraints_1 += [ nodes[0]['data'][idx_1] == nodes[2]['data'][idx_1 + idx_2]]
+        constraints_1 += [ nodes[0]['time'][idx_1] == nodes[2]['time'][idx_1 + idx_2]]
+        constraints_1 += [ nodes[0]['time'][idx_1] <  nodes[1]['time'][idx_2]]
+        constraints_2 += [ nodes[1]['data'][idx_2] == nodes[2]['data'][idx_1 + idx_2]]
+        constraints_2 += [ nodes[1]['time'][idx_2] == nodes[2]['time'][idx_1 + idx_2]]
+        constraints_2 += [ nodes[1]['time'][idx_2] <  nodes[0]['time'][idx_1]]
+        return Or(And(Merge(constraints_1), Merge1(nodes, idx_1 + 1, idx_2, bound)),
+                  And(Merge(constraints_2), Merge1(nodes, idx_1, idx_2 + 1, bound)))
+
+    
+    @staticmethod
+    def SyncDrain(nodes, bound):
+        assert len(nodes) == 2
         constraints = []
-        
         for i in range(bound):
-            
+            constraints += [nodes[0]['time'][i] == nodes[1]['time'][i]]
+
+        return Merge(constraints)
+    
+    
